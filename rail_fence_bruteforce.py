91930999286ee9d21fc65bbffddd4efbd2e5780d
Odyhibit <josh.2990@gmail.com>
2023-06-27 23:25:38 -0500

    Added a sort by length for the wordlist, and removed the words as they are found to prevent double counting words that extend other words. We look for longer words first then remove them onve they are counted.

--- rail_fence_bruteforce.py
+++ rail_fence_bruteforce.py
@@ -10,14 +10,28 @@ def main(cipher_text):
     if cipher_text is None:
         print("Nothing to solve, please give me some cipher text.")
         return
-    common_word_file = load_word_list("common_words_min_3_letters.txt")
-    row, offset = brute_force(cipher_text, common_word_file)
+    common_words_longest_first = load_word_list("common_words_min_3_letters.txt")
+    # print(common_words_longest_first)
+    row, offset, count_dict = brute_force(cipher_text, common_words_longest_first)
 
     print(f"Row count is most likely {green}{row}{reset} with offset {green}{offset}{reset}")
     print("Decoding with most likely settings . . .")
     print()
-    print(decode(cipher_text, row, offset))
-    print()
+
+    sorted_list = sorted(count_dict, key=count_dict.get, reverse=True)
+    # print(count_dict)
+    # print(sorted_list)
+    sorted_list.pop(0)
+    keep_going = True
+    while keep_going:
+        print(decode(cipher_text, row, offset))
+        print()
+        response = input(f"Does that look correct ({green}y{reset}/{green}n{reset})")
+        if response == "n" or response == "N":
+            row, offset = sorted_list.pop(0)
+            print(f"key:{row} offset:{offset} words found {count_dict[(row, offset)]}")
+        if response == "y" or response == "Y":
+            keep_going = False
 
 
 def decode(cipher: str, key: int, offset: int = 0) -> str:
@@ -33,6 +47,8 @@ def decode(cipher: str, key: int, offset: int = 0) -> str:
 
 
 def brute_force(cipher: str, wordlist: []):
+    cipher = cipher.lower()
+    word_count_dictionary = {}
     highest_word_count = 0
     row_candidate = 0
     offset_candidate = 0
@@ -44,13 +60,23 @@ def brute_force(cipher: str, wordlist: []):
             word_count = 0
             candidate = decode(cipher, key, offset)
             for word in wordlist:
-                if word in candidate:
-                    word_count += 1
+                word_occurrences, candidate = count_and_remove(word, candidate)
+                word_count += word_occurrences
             if word_count > highest_word_count:
                 highest_word_count = word_count
                 row_candidate = key
                 offset_candidate = offset
-    return row_candidate, offset_candidate
+            word_count_dictionary[(key, offset)] = word_count
+    return row_candidate, offset_candidate, word_count_dictionary
+
+
+def count_and_remove(needle: str, haystack: str, current_count: int = 0) -> (int, str):
+    index = haystack.find(needle)
+    if index == -1:
+        return current_count, haystack
+    new_haystack = haystack[:index] + haystack[index + len(needle):]
+    current_count += 1
+    return count_and_remove(needle, new_haystack, current_count)
 
 
 def load_word_list(path: str) -> []:
@@ -58,7 +84,7 @@ def load_word_list(path: str) -> []:
         common_word_list = []
         while line := common_words.readline():
             common_word_list.append(line.strip())
-        return common_word_list
+        return sorted(common_word_list, key=len, reverse=True)
 
 
 if __name__ == "__main__":
